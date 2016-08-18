21db489eb3fdd8ac7d0f0a3c96d4e9e958cc5ec2
truskulowylak <akarolak@mion.elka.pw.edu.pl>
2016-08-18 21:09:28 +0200

    Remove duplicated directory

--- Simulet/src/Protocol/Comm_Protocol.java
+++
@@ -1,7 +0,0 @@
-package Protocol;
-
-
-public abstract class Comm_Protocol {
-	public static final String SWITCHED_OFF = "OFF";
-	public static final String SWITCHED_ON = "ON";
-}
--- Simulet/src/modules/listOfAvailableModules.java
+++
@@ -1,7 +0,0 @@
-package modules;
-
-public abstract class listOfAvailableModules {
-
-	public static final String LAMPKA ="Lampka";
-	public static final String BUDZIK = "Budzik";
-}
